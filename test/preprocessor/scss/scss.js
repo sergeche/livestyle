2f3bc559a81b19373754111a1335e580ca9924bc
Sergey Chikuyonok <serge.che@gmail.com>
2014-02-14 19:13:27 +0400

    Working on SCSS resolver

---
+++ test/preprocessor/scss/scss.js
@@ -0,0 +1,50 @@
+var path = require('path');
+var assert = require('assert');
+var testUtils = require('../testUtils');
+var tree = require('../../../lib/tree');
+var diff = require('../../../lib/diff');
+var patch = require('../../../lib/patch');
+var locator = require('../../../lib/locator');
+
+var scssResolver = require('../../../lib/preprocessor/scss/resolver');
+var preprocessor = require('../../../lib/preprocessor/resolver');
+var selector = require('../../../lib/preprocessor/selector');
+
+function resolveSCSS(tree) {
+	return scssResolver.resolve(tree).filter(function(item) {
+		return isEmpty(item);
+	});
+}
+
+function resolveCSS(tree) {
+	return preprocessor.resolve(tree);
+}
+
+function p(dir) {
+	return path.join(__dirname, dir);
+}
+
+function isEmpty(item) {
+	// remove nodes with empty contents
+	return item.node.properties().filter(function(item) {
+		return item.name !== '&' && item.name.charAt(0) !== '@';
+	}).length;
+}
+
+function np(ix, path) {
+	return 'Rule ' + (ix + 1) + ': ' + selector.normalize(path.join(' / '));
+}
+
+describe('SCSS nesting', function() {
+	testUtils.getTreeSet(p('nesting'), 'scss').forEach(function(item) {
+		it('on file ' + item.preprocessorFile, function() {
+			var less = resolveSCSS(item.preprocessor);
+			var css = resolveCSS(item.css);
+
+			// console.log(_.pluck(less, 'path'));
+			less.forEach(function(item, i) {
+				assert.deepEqual(np(i, item.path), np(i, css[i].path));
+			});
+		});
+	});
+});
