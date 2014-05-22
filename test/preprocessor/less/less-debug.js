afcc38e2ec04b755d1960a9b8eef798885af6b1a
Sergey Chikuyonok <serge.che@gmail.com>
2014-05-23 02:00:40 +0400

    Working on LESS `extend` resolver

--- test/preprocessor/less/less-debug.js
+++ test/preprocessor/less/less-debug.js
@@ -3,6 +3,19 @@ var path = require('path');
 
 var lessResolver = require('../../../lib/preprocessor/less/resolver');
 
+function resolveLESS(tree) {
+	return lessResolver.resolve(tree).sectionList().filter(function(item) {
+		// check if current node contains empty sections
+		for (var i = 0, il = item.node.children.length, c; i < il; i++) {
+			c = item.node.children[i];
+			if (c.type == 'property' || c.children.length) {
+				return true;
+			}
+		}
+
+		return false;
+	});
+}
 
 describe('LESS debug', function() {
 	function p(dir) {
@@ -12,6 +25,9 @@ describe('LESS debug', function() {
 	it('should transform file', function() {
 		var less = fs.readFileSync(p('debug.less'), {encoding: 'utf8'});
 		var css = lessResolver.resolve(less);
-		console.log(css.toCSS());
+		console.log(css.toCSS(true));
+		// console.log(resolveLESS(less).map(function(item) {
+		// 	return item.name;
+		// }).join('\n'));
 	});
 })
